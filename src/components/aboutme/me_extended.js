070925bf558334c657ea9ea40db9d775524ed416
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-06-13 15:05:30 -0400

    code

---
+++ src/components/aboutme/me_extended.js
@@ -0,0 +1,8 @@
+import react from 'react'
+
+const AboutMeExtended = () => {
+    return <div className = about-me-page>
+    "A small price to pay for Salvation"
+    Full time fellow in the Software Engineering Immersive program that includes over 400 hours of professional training over 12 weeks. Utilizes hands-on approach to design and build web applications with full CRUD functionality through Javascript, Python, React, Express, SQL, PostgreSQL, HTML, CSS, Django.
+    </div>
+}
