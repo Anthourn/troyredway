813636fe7f761e25886dd371536ea7c447cbf45a
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-06-21 14:16:13 -0400

    resume added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
 import react from 'react'
-
+import Pdf from '../Resume_2021.pdf'
 const NavBar = () => {
     return (
         <div className= 'nav-bar'> <h1 id = 'my-name'>Troy Redway</h1>
         <a className = '' target="_blank" rel="noopener noreferrer" href = 'https://www.linkedin.com/in/troyredway/'> Linkedin</a>
         <a className = '' target="_blank" rel="noopener noreferrer"href = 'https://github.com/Anthourn'>Github</a>
-        <a href = '#'> Resume</a>
+        <a href = {Pdf} target="_blank"> Resume</a>
          </div>
     )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,20 +2,24 @@ import react from 'react'
 
 const Projects = () => {
     return(
+        <>
+        <h3>Projects</h3>
         <div className = 'projects'>
-        <div id = 'iron man' className = 'card' style={{backgroundImage: `url("https://geekireland.com/wp-content/uploads/2019/05/Avengers-logo-game.jpg")`}} >
+
+        <div className = 'card' style={{backgroundImage: `url("https://geekireland.com/wp-content/uploads/2019/05/Avengers-logo-game.jpg")`}} >
        <div className = 'card-content'>
        <h2 className = 'card-title'> Movies2Comics</h2>
        <p className = 'card-body'> Fan of the MCU? find the comics your favorite characters originated in.
        Built with with functional react, and marvel API.
        <br/>
        <img className='thumbnail' src="https://img.icons8.com/nolan/64/react-native.png" alt = 'react'/></p>
-       <a href = 'https://blooming-taiga-25990.herokuapp.com/' className = 'button'>Live</a>
+       <a href = 'https://blooming-taiga-25990.herokuapp.com/' target="_blank" className = 'button'>Live</a>
+       <a href = 'https://github.com/Anthourn/avengers' target="_blank" className = 'button'>Repo</a>
        </div>
 
        </div>
 
-       <div id = 'iron man' className = 'card' style={{backgroundImage: `url("https://pm1.narvii.com/7104/f1e0477e989ff43607193360191c0084820140d7r1-1241-704v2_hq.jpg")`}} >
+       <div  className = 'card' style={{backgroundImage: `url("https://pm1.narvii.com/7104/f1e0477e989ff43607193360191c0084820140d7r1-1241-704v2_hq.jpg")`}} >
        <div className = 'card-content'>
        <h2 className = 'card-title'> Spamazon</h2>
        <p className = 'card-body'>
@@ -37,12 +41,13 @@ const Projects = () => {
        <h2 className = 'card-title'> Apex gun guide</h2>
        <p className = 'card-body'> A mini wiki for weapons in the apex, built with express and MongoDB </p>
        <img className='thumbnail' src= 'https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg'/>
-       <a href = 'https://nxt-big-thing.herokuapp.com/guns' className = 'button'>Live</a>
-       <a href = 'https://github.com/Anthourn/Proj2' className = 'button'>Repo</a>
+       <a href = 'https://nxt-big-thing.herokuapp.com/guns' target="_blank" className = 'button'>Live</a>
+       <a href = 'https://github.com/Anthourn/Proj2' target="_blank" className = 'button'>Repo</a>
        </div>
        </div>
 
         </div>
+        </>
     )
 }
 
